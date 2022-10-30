2e24720f85885bb406a4a9e0826cc8f5bee71e1b
Simão Pedro Mendes Gabriel <spedromg@hotmail.com>
2022-10-30 11:36:16 -0300

    update app.py add bootstrap

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from flask import Flask, request
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask('__name__')
 
 @app.route('/')
 def index():
-  return '<h1>Hello World,  Flask Python! <a href="http://127.0.0.1:5000/user">usuário</a></h1>'
+  return render_template('index.html')
 
-def user():
-  return '<h1>Eu sou o usuário Simão Pedro.</h1>'
+@app.route('/users/<string:name>')
+def users(name):
+  return render_template('users.html', name=name)
 
-def user_id(name):
-  return f'<h1>Eu sou o usuário {name}.</h1>'
-
-def browsers():
-  user_agent =  request.headers.get('User-Agent')
-  return f'<h3>Seu browser é {user_agent}</h3>'
-
-app.add_url_rule('/user', 'user', user)
-app.add_url_rule('/user_id/<name>', 'user_id', user_id)
-app.add_url_rule('/browsers', 'browsers', browsers)
-
-if __name__ == "__main__":
-  app.run(debug=True)
+bootstrap = Bootstrap(app)
